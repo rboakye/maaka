a9f5acfcccf7fce773e7dc75002061c154f64b0a
Richmond B <richmond.boakye@flatironssolutions.com>
2014-01-01 21:12:19 -0700

    Signing in form validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ActiveRecord::Base
             :length => { :maximum => 100 },
             :format => EMAIL_REGEX,
             :confirmation => true
+
+  validates_format_of :email, without: /NOSPAM/
+
+  validates :password, length: { minimum: 6 },
+            if: :password_digest_changed?
+
+  validates :password_confirmation, presence: true, if: :password_digest_changed?
+
+  validates_exclusion_of :password, in: ->(user) { [user.login_name, user.first_name] },
+                         message: 'should not be the same as your login name or first name'
+
 end
