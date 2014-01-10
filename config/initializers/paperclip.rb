e6033d2cc83c38fa377d258ff719fb0dab713a49
Richmond B <richmond.boakye@flatironssolutions.com>
2014-01-10 03:22:34 -0700

    Few changes on paperclip configuration

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,7 +2,6 @@
 if Rails.env.production?
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
-Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 end
 
 if Rails.env.development?
