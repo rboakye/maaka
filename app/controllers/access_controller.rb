3b804db7a8d9b1653dac1df0610bf191e2dab965
Richmond B <richmond.boakye@flatironssolutions.com>
2014-01-15 03:05:36 -0700

    Fixed security hole. That allows the access using direct url path

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -17,35 +17,29 @@ class AccessController < ApplicationController
     if authorized_user
       # mark user as logged in
       session[:user_id] = authorized_user.id
-      set_access_params
+      session[:last_seen] = Time.now
       flash[:notice] = "#{authorized_user.first_name}, you are now logged in "
       redirect_to controller: 'users', action: 'index'
     else
       flash[:error] = "Invalid email/password combination."
-      redirect_to controller: 'users', action: 'index'
+      redirect_to controller: 'access', action: 'login_access'
     end
   end
 
   def logout
     # mark user as logged out
-    if session[:user_session] != nil
-      if session[:user_session].is_active
-        if session[:timed_out]
-          flash[:error] = "You being logged out"
-        else
-          flash[:notice] = "Logged out successfully"
-        end
-        session[:user_session].is_active = false
-      end
+    if @user_session.timed_out
+       flash[:error] = "You being logged out"
+    else
+      flash[:notice] = "Logged out successfully"
     end
     session[:user_id] = nil
     session[:last_seen] = nil
     session[:user_session] = nil
-    if session[:timed_out] = true
-      session[:timed_out] = false
+    if @user_session.timed_out
       redirect_to controller: 'access', action: 'login_access'
     else
-      redirect_to controller: 'users', action: 'index'
+      redirect_to root_path
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :confirm_valid_session, :except => [:attempt_alt_login, :logout, :login_access]
-  before_action :session_active, :except => [:attempt_alt_login,:login_access]
+  before_action :confirm_valid_session, :except => [:attempt_login, :login_access]
+  before_action :session_active, :except => [:attempt_login,:login_access, :logout]
 
   class UserSession
-    attr_accessor :user_id, :is_active
+    attr_accessor :user_id, :is_active, :timed_out
     def initialize(user_id)
       @user_id = user_id
       @is_active = false
+      @timed_out = false
     end
   end
 
@@ -19,18 +20,20 @@ class ApplicationController < ActionController::Base
   def confirm_valid_session
     if session[:user_id]
       if session[:user_session] == nil
-        session[:user_session] = UserSession.new(nil)
+        @user_session = UserSession.new(session[:user_id])
+        @user_session.is_active = true
+        session[:user_session] = @user_session
       else
         @user_session = session[:user_session]
       end
       user_info
       return true
-    else
-      if session[:user_session]
-        flash[:error] = 'login is required to access Makasa'
-        redirect_to controller: :access, action: :login_access
-      end
+    elsif request.original_url == root_url
       return false
+    else
+      flash[:error] = 'login is required to access Makasa'
+      @user_logged_in = false
+      redirect_to controller: :access, action: :login_access
     end
   end
 
@@ -49,7 +52,7 @@ class ApplicationController < ActionController::Base
     if session[:last_seen]
       if session[:last_seen] < 240.minutes.ago
         session[:last_seen] = Time.now
-        session[:timed_out] = true
+        @user_session.timed_out = true
         redirect_to controller: :access, action: :logout
       else
         session[:last_seen] = Time.now
@@ -59,11 +62,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def set_access_params
-    @user_session = UserSession.new(session[:user_id])
-    @user_session.user_id = session[:user_id]
-    @user_session.is_active = true
-    session[:user_session] = @user_session
-    session[:timed_out] = false
-  end
 end
